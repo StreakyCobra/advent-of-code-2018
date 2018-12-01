4c37132a3c4699b7381a0178edb963c9e4b911ef
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-12-01 11:51:28 +0100

    Add an `utils` module with a method that read file content.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::process;
 
+mod utils;
 mod prob_01;
 
 fn main() {
---
+++ src/utils.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::Read;
+use std::process;
+
+pub fn parse_file(name: &str) -> String{
+    let mut file = match File::open(name) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("The input file «{}» is missing", name);
+            process::exit(1)
+        }
+    };
+    
+    let mut content: String = String::new();
+    match file.read_to_string(&mut content) {
+        Ok(_) => (),
+        Err(_) => {
+            println!("The input file «{}» can not be read", name);
+            process::exit(1)
+        }
+    };
+
+    return content;
+}
