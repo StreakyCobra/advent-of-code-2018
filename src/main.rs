e2cfd046a9658199638eeaa3a1e8e119e5f0cdfc
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-12-01 11:05:13 +0100

    Main function that parse CLI args to solve given problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+mod prob_01;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Error, wrong number of arguments.\n");
+        println!("Usage: {} <problem_number>", args[0]);
+        process::exit(1);
+    }
+    let problem: Option<u8> = args[1].parse::<u8>().ok();
+    match problem {
+        Some(1) => prob_01::solve(),
+        Some(_) | None => {
+        println!("Problem solver not implemented yet.");
+        process::exit(1);
+        }
+    }
 }
